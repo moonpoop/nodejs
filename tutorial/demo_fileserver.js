c78689795246c147bf81a213dcdf5cb3266e4501
moon poop <moonpoop@protonmail.ch>
2020-12-20 19:13:30 -0500

    url module

---
+++ tutorial/demo_fileserver.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  // uses url module to parse filename and searches if filename exists and returns it if it does
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
